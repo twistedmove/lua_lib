ddd26cdd09f767a60aae8813ec870d48281afe2a
feng@necserver <zhfe99@gmail.com>
2015-08-10 22:30:05 -0700

    store

---
+++ img.lua
@@ -0,0 +1,58 @@
+#!/usr/bin/env th
+-- Lua image library.
+--
+-- History
+--   create  -  Feng Zhou (zhfe99@gmail.com), 08-10-2015
+--   modify  -  Feng Zhou (zhfe99@gmail.com), 08-10-2015
+
+local image = require 'image'
+local gm = require 'graphicsmagick'
+
+----------------------------------------------------------------------
+-- Split string into parts.
+--
+-- Input
+--   imgPath  -  file path
+--
+-- Ouput
+--   img      -  image, d x h x w
+function lua_lib.imgLoad(imgPath)
+  local ok, img = pcall(gm.Image, imgPath)
+
+  -- error
+  if not ok or img == nil then
+    print(imgPath .. ' is buggy')
+    os.exit()
+  end
+
+  -- convert to tensor
+  img = img:toTensor('float', 'RGB', 'DHW')
+
+  -- different format
+  if img:dim() == 2 then
+    img = img:reshape(1, img:size(1), img:size(2))
+  end
+  if img:size(1) == 1 then
+    img = torch.repeatTensor(img, 3, 1, 1)
+  end
+  if img:size(1) > 3 then
+    img = img[{{1, 3}, {}, {}}]
+  end
+
+  return img
+end
+
+----------------------------------------------------------------------
+-- Resize an image.
+--
+-- Input
+--   img0   -  original image
+--   sizMa  -  maximum size
+--
+-- Output
+--   img    -  new image
+function lua_lib.imgSizNew(img0, sizMa)
+  img = image.scale(img0, '^' .. sizMa)
+
+  return img
+end
--- init.lua
+++ init.lua
@@ -3,12 +3,13 @@
 --
 -- History
 --   create  -  Feng Zhou (zhfe99@gmail.com), 08-02-2015
---   modify  -  Feng Zhou (zhfe99@gmail.com), 08-09-2015
+--   modify  -  Feng Zhou (zhfe99@gmail.com), 08-10-2015
 
 lua_lib = {}
 
 include('io.lua')
 include('pr.lua')
 include('str.lua')
+include('img.lua')
 
 return lua_lib
